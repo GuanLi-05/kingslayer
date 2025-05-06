60930cff29b74f74deb4e4d9c9c47719ebb912d2
Guan Li <guanLi@student.unsw.edu.au>
2025-05-06 18:04:39 +1000

    page setup

---
+++ frontend/src/home_page/Encounter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Encounter() {
+  return (
+    <div>Encounter</div>
+  )
+}
+
+export default Encounter
---
+++ frontend/src/home_page/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Encounter from './Encounter'
+import Story from './Story'
+import Player from './Player'
+
+
+function Home() {
+  return (
+    <>
+      <Encounter />
+      <Story />
+      <Player />
+    </>
+  )
+}
+
+export default Home
